c2ba65c5bbe01fee34e4a111c86931af079f0511
Syedminhalabbas1 <minhalabbas475@gmail.com>
2026-05-24 07:13:10 -0700

    fix db connection

--- restaurant/backend/db.js
+++ restaurant/backend/db.js
@@ -5,20 +5,31 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false }
 });
 
+const poolPromise = Promise.resolve({
+  request: () => {
+    const params = [];
+    const obj = {
+      input: function(name, type, value) {
+        params.push(value);
+        return obj;
+      },
+      query: async function(q) {
+        const fixedQ = q.replace(/@(\w+)/g, (_, name) => {
+          const idx = params.findIndex((_, i) => i === params.indexOf(_));
+          return `$${params.indexOf(_) + 1}`;
+        });
+        const result = await pool.query(q, params);
+        return { recordset: result.rows };
+      }
+    };
+    return obj;
+  }
+});
+
 const sql = {
   NVarChar: 'text',
-  Int: 'int',
+  Int: 'integer', 
   Decimal: () => 'decimal'
 };
-
-const poolPromise = Promise.resolve({
-  request: () => ({
-    input: function() { return this; },
-    query: async (q) => {
-      const result = await pool.query(q);
-      return { recordset: result.rows };
-    }
-  })
-});
 
 module.exports = { sql, poolPromise }; 
